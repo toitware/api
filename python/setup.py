a540c66ec7165a733621c2e31b918b942c388ac5
Mark Medum Bundgaard <mmedum@gmail.com>
2020-10-28 15:08:46 +0100

    Removing comment

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="api", # Replace with your own username
+    name="api",
     version="0.0.1",
     author="Toitware",
     author_email="leon@toitware.com",
