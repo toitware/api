0931b5767887651cb83b227043afdb17c58be629
Jesper Lindstrøm <jesper@toitware.com>
2020-11-05 13:57:47 +0100

    Add required packages to python package

--- python/setup.py
+++ python/setup.py
@@ -13,11 +13,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/toitware/api",
-    packages=setuptools.find_packages(),
+    package_dir={'': 'src'},
+    packages=setuptools.find_namespace_packages(where='./src', include='toit.*'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=('protobuf','grpcio'),
     python_requires='>=3.6',
 )
